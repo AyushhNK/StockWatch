f0949685dc034424880724660f35e252c7f22dda
AyushhNK <neupaneayush3@gmail.com>
2025-12-24 18:18:07 +0545

    feat: Testing the app

--- config/settings/base.py
+++ config/settings/base.py
@@ -147,3 +147,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.User"
 
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
